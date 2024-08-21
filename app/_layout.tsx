dd007eb66b412016d5bfba04f6e33e531c6c2700
Fernando Herrera <fernando.herrera85@gmail.com>
2024-08-21 09:52:14 -0400

    chore: Remove unused files and dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,24 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Colors } from '@/constants/Colors';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+const RootLayout = () => {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
 
   if (!loaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <View style={{ backgroundColor: Colors.background, flex: 1 }}>
+      <Slot />
+
+      <StatusBar style="light" />
+    </View>
   );
-}
+};
+export default RootLayout;
---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const CalculatorApp = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 40, fontFamily: 'SpaceMono', color: 'white' }}>
+        CalculatorApp
+      </Text>
+    </View>
+  );
+};
+
+export default CalculatorApp;
